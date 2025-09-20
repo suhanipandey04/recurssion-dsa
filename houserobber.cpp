5bf19e984ecc53bec4891b59f4660de93695ace2
suhanipandey04 <dheeverse@gmail.com>
2025-09-20 12:48:00 +0530

    problem 3

---
+++ houserobber.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int solve(vector<int>& nums,int size, int index)
+    {
+        if(index>=size)
+        {
+            return 0;
+        }
+        int option1=nums[index]+solve(nums,size,index+2);
+
+        int option2=0+solve(nums,size,index+1);
+
+        int finalans=max(option1,option2);
+        return finalans;
+
+    }
+    int rob(vector<int>& nums) {
+        int size=nums.size();
+        int index=0;
+        int ans=solve(nums,size,index);
+        return ans;
+        
+
+        
+    }
+  
+};
